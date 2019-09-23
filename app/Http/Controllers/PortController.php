2e9681a838c15a71e9597b4e65652984df8c66a0
mauansen <3417474331@qq.com>
2019-09-23 18:19:47 +0800

    laravel

--- app/Http/Controllers/PortController.php
+++ app/Http/Controllers/PortController.php
@@ -41,7 +41,7 @@ class PortController extends Controller
         }
         if($xml_arr['EventKey'] == 'chaxun'){
             $point=DB::table('wechat_user')->where(['openid'=>$xml_arr['FromUserName']])->first();
-            $a='您的积分为'.$point;
+            $a='您的积分为'.$point->point;
         }
         $message = empty($a)? '欢迎关注！' : $a;
         $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
