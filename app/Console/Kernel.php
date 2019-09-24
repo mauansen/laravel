6206b996f94d954ffce9bdd7c9b3aef68b10b977
mauansen <3417474331@qq.com>
2019-09-24 16:51:18 +0800

    laravel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,36 +28,36 @@ class Kernel extends ConsoleKernel
     {
 
         $schedule->call(function(){
-//            $tools=new Tools();
-//            $data=DB::table('wechat_user')->get();
-//            $url='https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='.$tools->get_wechat_access_token();
-//            foreach($data as $v){
-//                if($v->or_sign==1){
-//                    $v->or_sign='未签到';
-//                }else{
-//                    $v->or_sign='已签到';
-//                }
-//                $array=[
-//                    'touser'=>$v->open_id,
-//                    'template_id'=>'TaKwcrDeSUqM365TbRbSxy67wbVzteYfPVuMQpsZmIU',
-//                    'data'=>[
-//                        'first'=>['value'=>'签到提醒'],
-//                        'keyword1'=>['value'=>$v->nickname],
-//                        'keyword2'=>['value'=>$v->or_sign],
-//                        'keyword3'=>['value'=>$v->points],
-//                        'keyword4'=>['value'=>$v->sign_time],
-//                    ]
-//                ];
-//                $tools->curl_post($url,json_encode($array,JSON_UNESCAPED_UNICODE));
-//
-//            }
-            DB::table('wechat_user')->where(['or_sign'=>2])->update([
-                'sign'=>'0'
-            ]);
+            $tools=new Tools();
+            $data=DB::table('wechat_user')->get();
+            $url='https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='.$tools->get_wechat_access_token();
+            foreach($data as $v){
+                if($v->or_sign==1){
+                    $v->or_sign='未签到';
+                }else{
+                    $v->or_sign='已签到';
+                }
+                $array=[
+                    'touser'=>$v->open_id,
+                    'template_id'=>'TaKwcrDeSUqM365TbRbSxy67wbVzteYfPVuMQpsZmIU',
+                    'data'=>[
+                        'first'=>['value'=>'签到提醒'],
+                        'keyword1'=>['value'=>$v->nickname],
+                        'keyword2'=>['value'=>$v->or_sign],
+                        'keyword3'=>['value'=>$v->points],
+                        'keyword4'=>['value'=>$v->sign_time],
+                    ]
+                ];
+                $tools->curl_post($url,json_encode($array,JSON_UNESCAPED_UNICODE));
+                DB::table('wechat_user')->where(['or_sign'=>2])->update([
+                    'sign'=>'0'
+                ]);
+            }
+
             \Log::info('123');
             DB::table('wechat_user')->update([
                 'or_sign'=>2,
-                'sign_time'=>''
+                'sign_time'=>'1'
             ]);
         })->cron('* * * * *');
 //        $schedule->call(function(){
